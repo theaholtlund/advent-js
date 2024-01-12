96b712886050255140ce22092c41258c5ee4ad5d
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-12 23:27:07 +0100

    Add basis for the day seven challenge

---
+++ day-7.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 7
+
+/**
+ * Challenge: The 3D Boxes
+ * Objective: Generate a drawing of a 3D gift using ASCII characters, given an integer size.
+ *
+ */
+function drawGift(size, symbol) {
+  // Add code here
+  return "#\n";
+}
