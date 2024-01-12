b37eeacfffc3543a0c33c6c81bc2e10c9bfeef4e
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-12 16:11:54 +0100

    Add solution to the day six challenge

---
+++ day-6.js
@@ -0,0 +1,28 @@
+// Advent JavaScript 2023, Day 6
+
+/**
+ * Challenge: The Reindeer on Trial
+ * Objective: Create simulation of sled's movement, returning representation of the road state
+ *
+ * @param {string} movements - Characters representing the direction of reindeer's movement.
+ * @return {number} - Value of the maximum distance that the reindeer can travel in any direction.
+ */
+function maxDistance(movements) {
+  let leftCount = 0;
+  let rightCount = 0;
+  let anyDirectionCount = 0;
+
+  // Iterate through each movement
+  for (let move of movements) {
+    if (move === ">") {
+      rightCount++;
+    } else if (move === "<") {
+      leftCount++;
+    } else if (move === "*") {
+      anyDirectionCount++;
+    }
+  }
+
+  // Calculate left and right difference, plus any direction count
+  return Math.abs(leftCount - rightCount) + anyDirectionCount;
+}
