b7dd5f72b3c9a3799d375dd8879a41a22c512b4d
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-13 13:47:19 +0100

    Add basis for the day nine challenge

---
+++ day-9.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 9
+
+/**
+ * Challenge: Switch the lights
+ * Objective: Given array of strings with light colours, return minimum number of light changes for colors to alternate
+ *
+ */
+function adjustLights(lights) {
+  // Add code here
+  return 0;
+}
