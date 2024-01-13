037b3460d8cc01a94192aed5a4e02473d2d26cef
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-13 09:04:28 +0100

    Add solution to day seven challenge

--- day-7.js
+++ day-7.js
@@ -9,23 +9,45 @@
  * @return {string} - String representing the ASCII art of the 3D gift.
  */
 function drawGift(size, symbol) {
+  // Constants for wrapper and space characters
+  const wrapper = "#";
+  const space = " ";
+
+  // Base case: If the size is 1 or less, return a simple wrapper
+  if (size <= 1) return `${wrapper}\n`;
+
+  // Initialise an empty string to store the ASCII art of the 3D gift
   let gift = "";
 
-  for (let i = 0; i < size; i++) {
-    gift +=
-      " ".repeat(size - i - 1) +
-      "#".repeat(i + 1) +
-      symbol.repeat(size - i - 1) +
-      "\n";
+  // Arrays to store the top and bottom parts of the gift
+  const top = [space.repeat(size - 1) + wrapper.repeat(size)];
+  const bottom = [`${wrapper.repeat(size)}`];
+
+  // Middle part of the gift with symbols
+  const middle =
+    `${wrapper.repeat(size)}${symbol.repeat(Math.abs(size - 2))}` +
+    `${wrapper}\n`;
+
+  // Loop to generate the top and bottom parts of the gift
+  for (let i = 1; i < size; i++) {
+    // Construct line for each iteration
+    const line =
+      `${wrapper}${symbol.repeat(size - 2)}${wrapper}` +
+      `${symbol.repeat(i - 1)}${wrapper}`;
+
+    // Add lines to the top and bottom arrays
+    top.push(space.repeat(size - i - 1) + line);
+    bottom.push(line);
   }
 
-  for (let i = size - 2; i >= 0; i--) {
-    gift +=
-      " ".repeat(size - i - 1) +
-      "#".repeat(i + 1) +
-      symbol.repeat(size - i - 1) +
-      "\n";
-  }
+  // Combine and assemble top and bottom parts to construct 3D gift
+  top.pop();
+  bottom.pop();
+  top.push(middle);
+  bottom.reverse();
+
+  // Concatenate top and bottom arrays, join with line breaks and add newline to the end
+  gift = `${top.join("\n")}${bottom.join("\n")}\n`;
 
   return gift;
 }
