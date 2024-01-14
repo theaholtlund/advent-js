4b3a1e895bbc57578df6b185975d1ee06f0d2655
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-14 16:47:26 +0100

    Add outline for day ten challenge

---
+++ day-10.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 10
+
+/**
+ * Challenge: Create your own Christmas tree
+ * Objective: Create Christmas tree given string to form tree and number that indicates its height
+ *
+ */
+function createChristmasTree(ornaments, height) {
+  // Add code here
+  return "|";
+}
