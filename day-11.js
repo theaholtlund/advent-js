7101fe63c28c34ae16341dff2b0cebb5575361bd
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-15 08:58:57 +0100

    Add outline to day 11 challenge

---
+++ day-11.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 11
+
+/**
+ * Challenge: The studious elves
+ * Objective: Determine if a palindrome can be formed by making at most one exchange of letters
+ *
+ */
+function getIndexsForPalindrome(word) {
+  // Add code here
+  return [0, 0];
+}
