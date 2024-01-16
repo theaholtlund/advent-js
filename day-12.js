6c3ddccdc9454caf28ecb1176c59e1a1cf63f0c0
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-16 07:03:13 +0100

    Create outline for day 12 challenge

---
+++ day-12.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 12
+
+/**
+ * Challenge: Is it a valid copy?
+ * Objective: Detect if a letter is a copy of another with degradation
+ *
+ */
+function checkIsValidCopy(original, copy) {
+  // Add code here
+  return true;
+}
