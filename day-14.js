4fc6ddbc8bc0745a1844e51fd8dbdddaf41a5e9a
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-17 23:06:18 +0100

    Create outline for day 14 challenge

---
+++ day-14.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 14
+
+/**
+ * Challenge: Avoid the alarm
+ * Objective: Avoid consecutive houses to maximise gift delivery, by calculating total gifts possible
+ *
+ */
+function maxGifts(houses) {
+  // Add code here
+  return 0;
+}
