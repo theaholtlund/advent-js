0fa7ef531ecade5b9ecda6a55f7a323c746b379f
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-17 23:02:38 +0100

    Add outline for day 13 challenge

---
+++ day-13.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 13
+
+/**
+ * Challenge: Calculating the time
+ * Objective: Return time difference between 07:00:00 and duration of Christmas Eve deliveries
+ *
+ */
+function calculateTime(deliveries) {
+  // Add code here
+  return "00:00:00";
+}
