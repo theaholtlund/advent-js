01a30578bad6b5fe7bca786a8db6eae5d0e181ec
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-19 11:08:47 +0100

    Add outline for solving day 16 challenge

---
+++ day-16.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 16
+
+/**
+ * Challenge: Friday deployment
+ * Objective: Convert Christmas tree array into an object, handling null values and maintaining relationships
+ *
+ */
+function transformTree(tree) {
+  // Add code here
+  return tree;
+}
