e350b2a10474aa6549a5bfe0b631e11bfcc2893e
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-19 10:29:41 +0100

    Add outline for day 15 challenge

---
+++ day-15.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 15
+
+/**
+ * Challenge: Autonomous robot
+ * Objective: Guide robots through warehouses, and return final state with the robot's position.
+ *
+ */
+function autonomousDrive(store, movements) {
+  // Add code here
+  return store;
+}
