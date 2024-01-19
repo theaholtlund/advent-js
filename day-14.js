c971c415d8ff9465e4dc5548a10c7eb51530b8a5
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-19 10:26:20 +0100

    Clean up comments to make them more consise

--- day-14.js
+++ day-14.js
@@ -13,25 +13,25 @@ function maxGifts(houses) {
     return 0;
   }
 
-  // Initialise two variables to keep track of maximum gifts with and without the current house
+  // Initialise two variables to keep track of maximum gifts with and without current house
   let maxWithCurrentHouse = houses[0];
   let maxWithoutCurrentHouse = 0;
 
-  // Iterate through the houses starting from the second one
+  // Iterate through houses starting from the second one
   for (let i = 1; i < houses.length; i++) {
     // Calculate the new maximum gifts with the current house
     const newMaxWithCurrentHouse = maxWithoutCurrentHouse + houses[i];
 
-    // Update variable to be the maximum of the previous two values
+    // Update variable without current house to be the maximum of the previous two values
     maxWithoutCurrentHouse = Math.max(
       maxWithoutCurrentHouse,
       maxWithCurrentHouse
     );
 
-    // Update maxWithCurrentHouse to be the new maximum with the current house
+    // Update variable to be the new maximum with the current house
     maxWithCurrentHouse = newMaxWithCurrentHouse;
   }
 
-  // Return the maximum of the two variables representing the total gifts
+  // Return maximum of the two variables representing total gifts
   return Math.max(maxWithCurrentHouse, maxWithoutCurrentHouse);
 }
