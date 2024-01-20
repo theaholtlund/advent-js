0285d6223251d677db991f10219c33b34c8700f2
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-20 16:09:52 +0100

    Add outline for day 17 challenge

---
+++ day-17.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 17
+
+/**
+ * Challenge: Optimising the rental
+ * Objective: Merge overlapping time intervals for sled rentals and return sorted array of optimised intervals
+ *
+ */
+function optimizeIntervals(intervals) {
+  // Add code here
+  return [];
+}
