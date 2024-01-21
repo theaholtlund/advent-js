4851a03a9c6128cd4d6b637392f44e3f4f3532d3
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-21 09:44:58 +0100

    Add outline for day 18 challenge

---
+++ day-18.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 18
+
+/**
+ * Challenge: The digital clock
+ * Objective: Create visual representation of 'HH:MM' time on digital clock with specific asterisk and blank space patterns
+ *
+ */
+function drawClock(time) {
+  // Add code here
+  return [];
+}
