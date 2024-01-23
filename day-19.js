69679d8f40403d23cc4dd5bd2f45226baf7ce494
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-23 09:23:58 +0100

    Add outline for day 19 challenge

---
+++ day-19.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 19
+
+/**
+ * Challenge: Face the sabotage
+ * Objective: Modify a matrix, replacing safe cells with count of adjacent sabotaged toys
+ *
+ */
+function revealSabotage(store) {
+  // Add code here
+  return [];
+}
