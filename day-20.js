0389c44b1a6ba8a0ec91126a18fb69e7ab265bf9
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-01-24 22:29:39 +0100

    Add outline for day 20 challenge

---
+++ day-20.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 20
+
+/**
+ * Challenge: Distribute the weight
+ * Objective: Adjust distribution matrix by replacing each element with rounded average of itself and its non-diagonal neighbors
+ *
+ */
+function distributeGifts(weights) {
+  // Add code here
+  return weights;
+}
