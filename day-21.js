757b543bfe02e2c91e62963ac6df394bb75bba89
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-25 07:39:33 +0100

    Add outline for day 21 challenge

---
+++ day-21.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 21
+
+/**
+ * Challenge: Binary message
+ * Objective: Pinpoint longest segment in a binary message array with equal count of 0s and 1s, aiding elves in decoding messages from Mars for potential communication via https://mars.codes
+ *
+ */
+function findBalancedSegment(message) {
+  // Add code here
+  return [];
+}
