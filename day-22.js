0ba9941dc135ceb26f1c651045aa51ab586b5a29
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-26 08:36:49 +0100

    Add comments to working solution

--- day-22.js
+++ day-22.js
@@ -8,19 +8,23 @@
  * @returns {number} - The result of executing the compiled code
  */
 function compile(code) {
+  // Initialise counter to track the result of code execution
   let counter = 0;
   let returnIndex = null;
 
+  // Evaluate symbols to update counter and return index accordingly
   for (let i = 0; i < code.length; i++) {
     counter += (code[i] === "+") - (code[i] === "-");
     counter *= (code[i] === "*") + 1;
     returnIndex = code[i] === "%" ? i : returnIndex;
 
     if (returnIndex !== null && code[i] === "<") {
+      // Return to last '%' symbol's index if '<' symbol is encountered
       i = returnIndex;
       returnIndex = null;
     }
 
+    // Skip conditional block if counter is non-positive and '¿' symbol is encountered
     if (counter <= 0 && code[i] === "¿") {
       const endIndex = code.indexOf("?", i + 1);
       if (endIndex !== -1) {
