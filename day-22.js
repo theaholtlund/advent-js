9ac58faa3476b93873061fff9340723151a8e1cd
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-26 08:27:15 +0100

    Add outline for day 22 challenge

---
+++ day-22.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 22
+
+/**
+ * Challenge: Programming language
+ * Objective: Create a compiler for Santa.js, a symbol-based language, modifying a counter value
+ *
+ */
+function compile(code) {
+  // Add code here
+  return 0;
+}
