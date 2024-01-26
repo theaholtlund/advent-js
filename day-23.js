db2b08e62c429eba57afa9bc695be243c580d4ca
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-26 08:41:40 +0100

    Add outline for day 23 challenge

---
+++ day-23.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 23
+
+/**
+ * Challenge: Christmas dinner
+ * Objective: Group Christmas dishes by shared ingredients, ensuring alphabetical ordering and uniqueness in the menu
+ *
+ */
+function organizeChristmasDinner(dishes) {
+  // Add code here
+  return [];
+}
