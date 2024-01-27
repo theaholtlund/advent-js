d431d703769c9abab79a9cddb384b148bcda0c55
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-01-27 16:37:42 +0100

    Add outline for day 24 challenge

---
+++ day-24.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 24
+
+/**
+ * Challenge: Jump on the stairs
+ * Objective: List all possible jump sequences to ascend a staircase within a given maximum, in ascending order
+ *
+ */
+function getStaircasePaths(steps, maxJump) {
+  // Add code here
+  return [];
+}
