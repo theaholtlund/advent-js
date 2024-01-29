ce3c394866eb5f4b55843c2e5b304c2c74b15b82
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-01-29 11:33:02 +0100

    Add outline for day 25 challenge

---
+++ day-25.js
@@ -0,0 +1,11 @@
+// Advent JavaScript 2023, Day 25
+
+/**
+ * Challenge: Calculating distances
+ * Objective: Compute total travel distance to deliver gifts, given sequential positions on a map where each move equals 1 km
+ *
+ */
+function travelDistance(map) {
+  // Add code here
+  return 0;
+}
